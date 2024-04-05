313a05043525935540e7780b00b97f82e3ab3e0a
atikfaisalemon <atikfaisalemon@gmail.com>
2024-04-05 15:58:25 +0600

    all working

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 import "./style.css";
 
-const submit = document.getElementById("btn");
-const answerElement = document.getElementsByClassName("checked");
-let score = 0;
+// const submit = document.getElementById("btn");
+// const answerElement = document.getElementsByClassName("checked");
 
-submit.addEventListener("click", checkedAns);
+const questionNo1 = "dhaka";
+const questionNo2 = "bill";
+const questionNo3 = "california";
+const questionNo4 = "iPhone15";
 
-function checkedAns() {
-  const checked = document.querySelector('input[type="radio"]:checked');
-  const anss = checked.nextElementSibling.textContent;
-  console.log(anss);
+const question_form = document.getElementById("question_form");
 
-  if (anss === "iPhone 15") {
-    score++;
-  } else {
-    document.getElementById("finalScore").innerHTML = score;
-  }
-}
+question_form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let score = 0;
+  const formData = new FormData(question_form);
+
+  const capital_ans = formData.get("capital");
+  const founder_ans = formData.get("founder");
+  const location_ans = formData.get("location");
+  const phone_ans = formData.get("phone");
+
+  if (capital_ans === questionNo1) score++;
+  if (founder_ans === questionNo2) score++;
+  if (location_ans === questionNo3) score++;
+  if (phone_ans === questionNo4) score++;
+
+  console.log("score", score);
+  document.getElementById("finalScore").textContent = score;
+});
+
+// submit.addEventListener("click", checkedAns);
+
+// function checkedAns() {
+//   const checked = document.querySelectorAll('input[type="radio"]:checked');
+//   console.log(checked.values);
+// }
